b369f9aa3348d504431d35efc75f48db664d9de4
Wardal <igor.dubovik@outlook.com>
2017-10-30 23:51:29 +0200

    Add redirect from "home" / to application index

--- OAuth2DjangoSampleApp/urls.py
+++ OAuth2DjangoSampleApp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(pattern_name='sampleAppOAuth2:index')),
     url(r'^(?i)sampleAppOAuth2/', include('sampleAppOAuth2.urls', namespace='sampleAppOAuth2')),
     url(r'^(?i)admin/', admin.site.urls),
 ]
